2654ddc1b7b1b972a18544ab2e81399744a67ee3
Cedric K. Wachira <cedric.kiama@gmail.com>
2023-10-31 23:16:26 +0400

    Added must know terminologies for Data Engineering

---
+++ 4.Must_Know_Data_Eng.sh
@@ -0,0 +1,9 @@
+1️⃣ Data Warehouse: Centralized repository for structured, historical data.
+2️⃣ Data Mart: Subset of a data warehouse, focused on specific business area.
+3️⃣ Data Lake: Storehouse for vast amounts of raw data, structured and unstructured.
+4️⃣ Delta Lake: Data lake with ACID transactions, ensuring data integrity.
+5️⃣ Data Pipeline: Processes that move and transform data from source to destination.
+6️⃣ Data Mesh: Decentralized data architecture, promoting domain-oriented ownership.
+7️⃣ Data Lakehouse: Hybrid model combining features of data lakes and warehouses.
+8️⃣ Data Swamp: Data lake with poor quality, unmanaged data.
+9️⃣ Data Fabric: Integrated data environment enabling seamless data access and sharing.
